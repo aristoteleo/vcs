e78fed342e8f5648a2e6ed403f8f69c64af2f217
Jason Boutte <boutte.jason@gmail.com>
2020-05-21 03:25:48 -0700

    Fixes antialiasing test, removing dependency on hardward

--- tests/test_vcs_antialiasing.py
+++ tests/test_vcs_antialiasing.py
@@ -6,7 +6,13 @@ class TestVCSAntialias(unittest.TestCase):
 
     def testAntiAliasing(self):
         x = vcs.init()
-        x.open()
+        # Calling `open` creates the vtkRenderWindow and calls `Render`. This call will evaulate the systems
+        # capabilities and set antialiasing to the requested value as long as its less than or equal the maximum
+        # value supported by the system. A scenario exists where vcs will set the initial value to 8 and
+        # the system isn't capable of this and the first call to getantialiasing after open will result in
+        # 0 being returned.
+        # x.open()
+
         # test it is on by default
         self.assertEqual(x.getantialiasing(), 8)
 
